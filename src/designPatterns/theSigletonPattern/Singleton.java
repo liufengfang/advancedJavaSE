e9d61a7ffa426e499fbf23e1654fa066faa2f395
liufengfang <liufengfang2017@icloud.com>
2018-07-09 22:32:27 +0800

    单例模式

---
+++ src/designPatterns/theSigletonPattern/Singleton.java
@@ -0,0 +1,35 @@
+package designPatterns.theSigletonPattern;
+
+/**
+ * Created by liufengfang on 2018/7/9.
+ */
+public class Singleton {
+    private String name;
+
+    //    public static Singleton UNIQUEINSTANCE = null;
+    private static Singleton UNIQUEINSTANCE;
+
+    private Singleton() {
+    }
+
+    private Singleton(String name) {
+        this.name = name;
+    }
+
+    //    static {
+//        UNIQUEINSTANCE = new Singleton();
+//    }
+    private Singleton getInstainceWithName() {
+        if (null == UNIQUEINSTANCE) {
+            UNIQUEINSTANCE = new Singleton();
+        }
+        return UNIQUEINSTANCE;
+    }
+
+    private Singleton getInstainceWithName(String name) {
+        if (null == UNIQUEINSTANCE) {
+            UNIQUEINSTANCE = new Singleton(name);
+        }
+        return UNIQUEINSTANCE;
+    }
+}
