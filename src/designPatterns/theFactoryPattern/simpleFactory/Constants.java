0718daca520a0f8a3cd130ae5af765827f2a8c1b
liufengfang <liufengfang2017@icloud.com>
2019-04-16 16:23:44 +0800

    工厂模式，优化常量类

--- src/designPatterns/theFactoryPattern/simpleFactory/Constants.java
+++ src/designPatterns/theFactoryPattern/simpleFactory/Constants.java
@@ -4,6 +4,6 @@ package designPatterns.theFactoryPattern.simpleFactory;
  * Created by liufengfang on 2018/7/8.
  */
 public class Constants {
-    private Constants(){};
-    public static enum pizzaSize {MINI,MIDDLE,MAX};
+    private Constants(){}
+    public enum pizzaSize {MINI,MIDDLE,MAX}
 }
