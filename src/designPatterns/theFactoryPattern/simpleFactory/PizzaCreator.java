134492fe6c0088d014b10fdf3dfd7cd987d90881
liufengfang <liufengfang2017@icloud.com>
2019-04-16 17:07:05 +0800

    简单工厂模式，添加注释

--- src/designPatterns/theFactoryPattern/simpleFactory/PizzaCreator.java
+++ src/designPatterns/theFactoryPattern/simpleFactory/PizzaCreator.java
@@ -3,7 +3,10 @@ package designPatterns.theFactoryPattern.simpleFactory;
 /**
  * Created by liufengfang on 2018/7/8.
  */
-//简单工厂
+//简单工厂模式(Simple Factory Pattern)，通过参数的不同返回不同的实例，被创建的父类通常有共同的父类。
+//    因为经常是静态的，所以也叫静态工厂方法模式(Static Factory Method Pattern)
+//工厂模式即工厂方法模式，有一个'大哥'是抽象工厂模式。
+
 public class PizzaCreator {
 
     public Pizza create(String name, Constants.pizzaSize size){
